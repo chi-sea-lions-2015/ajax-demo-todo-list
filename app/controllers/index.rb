562bc9f72394b85c953d69204cae240f7a4aea2a
Lucas Willett <me@ltw.io>
2015-03-25 10:19:25 -0500

    make go ajax

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,9 +1,14 @@
 get '/' do
+  sleep 2
   @items = Item.all
   erb :index
 end
 
 post '/items' do
   @item = Item.create(params[:item])
-  redirect '/'
+  if request.xhr?
+    erb :"_item", locals: {item: @item}, layout: false
+  else
+    redirect '/'
+  end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,2 +1,17 @@
 $(document).ready(function() {
+  $("#new_item").submit(function(e){
+    e.preventDefault();
+
+    var form = $(this);
+    var request = $.ajax({
+      method: form.attr('method'),
+      url: form.attr('action'),
+      data: form.serialize()
+    });
+
+    request.done(function(response) {
+      $("#todo-list").append(response);
+      form[0].reset();
+    });
+  });
 });
